f0e692d744fe3ed8f5d858cb66835571d3fdf479
Ricardo Flores <rik.fm1809@gmail.com>
2023-04-26 00:46:01 -0500

    Modify workflow 00:45

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ const server = buildApp()
 
 async function main (){
    try {
-      await server.listen({port:PORT, host: "0.0.0.0"})
-      console.log(`Server running at http://localhost:${PORT}`);
+      await server.listen({port:process.env.PORT})
+      console.log(`Server running at http://localhost:${process.env.PORT}`);
    } catch (err) {
       console.log( err );
       process.exit(1)
