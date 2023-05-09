90fa832775bc37286019829756deab945f61eee7
loxi1 <anibal.cayetano@gmail.com>
2023-05-09 12:27:28 -0500

    Add funcionalidad editar perfil y editar passwor

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,10 +1,11 @@
 import { type FastifyInstance } from "fastify";
-import { loginHandler, registerUserHandler, getUsersHandler } from "./users.controller";
+import { loginHandler, registerUserHandler, getUsersHandler, updateUser, updatePassword } from "./users.controller";
 import { $ref } from "./users.schemas";
 
 async function UserRoutes(app: FastifyInstance){
    app.post("/register", {
       schema: {
+         tags: ['User'],
          body: $ref("createUserSchema"),
          response: {
             201: $ref("createUserResponseSchema")
@@ -13,17 +14,41 @@ async function UserRoutes(app: FastifyInstance){
    }, registerUserHandler)
    
    app.get("/",{ 
-      preHandler: [app.authenticate]
+      preHandler: [app.authenticate],
+      schema: {
+         tags: ['User'],
+      }
    }, getUsersHandler)
 
    app.post("/login", {
       schema: {
+         tags: ['User'],
          body: $ref('loginSchema'),
          response: {
             200: $ref("loginResponseSchema")
          }
       }
    }, loginHandler)
+
+   app.post("/perfil", {
+      schema: {
+         tags: ['User'],
+         body: $ref('updateUserSchema'),
+         response: {
+            200: $ref('responseOkSchema')
+         }
+      }
+   }, updateUser)
+
+   app.post("/changepassword", {
+      schema: {
+         tags: ['User'],
+         body: $ref('updatePasswordSchema'),
+         response: {
+            200: $ref('responseOkSchema')
+         }
+      }
+   }, updatePassword)
 }
 
 
--- src/modules/users/users.schemas.ts
+++ src/modules/users/users.schemas.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { string, z } from "zod";
 import { buildJsonSchemas } from "fastify-zod";
 
 const userCore = {
@@ -13,6 +13,20 @@ const userCore = {
 
 const createUserSchema = z.object({
     ...userCore,
+    password: z.string({
+        required_error: "Requiere password",
+        invalid_type_error: "La contraseña debe ser cadena"
+    })
+})
+
+const updateUserSchema = z.object({
+    id: z.number(),
+    ...userCore,
+    current_location: string()
+})
+
+const updatePasswordSchema = z.object({
+    id: z.number(),
     password: z.string({
         required_error: "Requiere password",
         invalid_type_error: "La contraseña debe ser cadena"
@@ -36,13 +50,23 @@ const loginResponseSchema = z.object({
     accessToken: z.string()
 })
 
+const responseOkSchema = z.object({
+    code: z.boolean(),
+    msn: z.string()
+})
+
 export type CreateUserInput = z.infer<typeof createUserSchema>
+export type UpdateUserSchema = z.infer<typeof updateUserSchema>
+export type UpdatePasswordSchema = z.infer<typeof updatePasswordSchema>
 
 export type LoginInput = z.infer<typeof loginSchema>
 
 export const {schemas: userSchema, $ref } = buildJsonSchemas({
     createUserSchema,
     createUserResponseSchema,
+    updateUserSchema,
+    updatePasswordSchema,
     loginSchema,
-    loginResponseSchema
+    loginResponseSchema,
+    responseOkSchema
 }, {$id:'User_Schemas'})
